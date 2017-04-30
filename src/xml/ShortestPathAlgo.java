84c7d6ab006b0941e396d64349f5ce5de5cee2c2
mike <mike@mikes-MacBook-Pro.local>
2017-04-30 14:27:23 -0500

    Updated Proj

--- src/xml/ShortestPathAlgo.java
+++ src/xml/ShortestPathAlgo.java
@@ -11,11 +11,68 @@ public class ShortestPathAlgo {
 	ArrayList<String> lowPath = new ArrayList(); 
 	
 	public static void findBestPath(start, end){
+		//start is the beginning
 		mapPairs(start);
+		ArrayList<String> pathList = new ArrayList();
+		pathList.add(start);
+		findPaths(start,end,pathList);
 	}
 	
-	public void mapPairs(init){
-		seen.add(init);
+	//facName is declared in xmlreader.java.... I am thinking
+	//that I need to put this into an interface
+	public void mapPairs(facName){
+		seen.add(facName);
+		//should be linkCity is also declared in xmlreader.java 
+		//use a loop to get all the neighbors
+		for(String linkCity:facName){
+			getDistance(linkCity,facName);
+			pairs.add(facName, linkCity, distance);
+			if(!seen.contains(linkCity)){
+				mapPairs(linkCity);
+				}	
+			return;
+			}
+		return;
+		}
+	
+	
+	private static void findPath(start, end, pathList){
+		if(start.equals(end)){
+			if(lowPath.empty()){
+				lowPath = pathList;
+				return;
+			}
+			else{
+				if(sum(pathList.getDistance()) < sum(lowPath.getDistance())){
+					lowPath = pathList;
+				}
+				else{
+					return;
+				}
+			}
+		}
+		else{
+			Set<String> fromHere = new HashSet<String>();
+			for(String pairing : pairs){
+				if(pairs[0].equals(start)){
+					fromHere.add(start);
+				}
+			}
+			for(String pairing : fromHere){
+				if(!pathList.contains(pairs.next())){
+					ArrayList<String> newPath = pathList.clone();
+					newPath.add(pairs.next());
+					findPaths(pairs.next(), end, newPath);
+				}
+				return;
+			}
+			
+		}
+	}
+	
+	public static void getDistance(linkCity, facName){
+		int distance = linkCity - facName;
+		return distance;
 	}
 	public static void main(String[] args) {
 		
